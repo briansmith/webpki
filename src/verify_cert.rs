09734f4e5ba63b67183c69f0dcb470a00bd7a2e2
Brian Smith <brian@briansmith.org>
2015-08-20 07:28:08 -0400

    Validate basic constraints.

--- src/verify_cert.rs
+++ src/verify_cert.rs
@@ -13,6 +13,7 @@
 // OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
 
 use super::Error;
+use super::cert::EndEntityOrCA;
 use super::der;
 use super::input::Reader;
 use time::Timespec;
@@ -37,4 +38,47 @@ fn check_validity(input: &mut Reader, time: Timespec) -> Result<(), Error> {
     // do something similar.
 
     Ok(())
+}
+
+#[derive(Clone, Copy)]
+enum UsedAsCA { Yes, No }
+
+fn used_as_ca(ee_or_ca: EndEntityOrCA) -> UsedAsCA {
+    match ee_or_ca {
+        EndEntityOrCA::EndEntity => UsedAsCA::No,
+        EndEntityOrCA::CA(..) => UsedAsCA::Yes
+    }
+}
+
+// https://tools.ietf.org/html/rfc5280#section-4.2.1.9
+fn check_basic_constraints(input: Option<&mut Reader>, used_as_ca: UsedAsCA,
+                           sub_ca_count: usize) -> Result<(), Error> {
+    let (is_ca, path_len_constraint) = match input {
+        Some(input) => {
+            let is_ca = try!(der::optional_boolean(input));
+
+            // https://bugzilla.mozilla.org/show_bug.cgi?id=985025: RFC 5280
+            // says that a certificate must not have pathLenConstraint unless
+            // it is a CA certificate, but some real-world end-entity
+            // certificates have pathLenConstraint.
+            let path_len_constraint =
+                if !input.at_end() {
+                    let value = try!(der::integer(input));
+                    Some(value as usize)
+                } else {
+                    None
+                };
+
+            (is_ca, path_len_constraint)
+        },
+        None => (false, None)
+    };
+
+    match (used_as_ca, is_ca, path_len_constraint) {
+        (UsedAsCA::No, true, _) => Err(Error::CAUsedAsEndEntity),
+        (UsedAsCA::Yes, false, _) => Err(Error::EndEntityUsedAsCA),
+        (UsedAsCA::Yes, true, Some(len)) if sub_ca_count > len =>
+            Err(Error::PathLenConstraintViolated),
+        _ => Ok(())
+    }
 }
