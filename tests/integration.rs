8e1eea0670718d636062da4c72b15452b1ac4524
Brian Smith <brian@briansmith.org>
2017-08-27 12:18:23 -1000

    Fix `use` style in tests/integration.rs.

--- tests/integration.rs
+++ tests/integration.rs
@@ -16,19 +16,17 @@ extern crate untrusted;
 extern crate webpki;
 extern crate ring;
 
-use untrusted::Input;
-use webpki::*;
-
-static ALL_SIGALGS: &'static [&'static SignatureAlgorithm] = &[
-    &ECDSA_P256_SHA256,
-    &ECDSA_P256_SHA384,
-    &ECDSA_P384_SHA256,
-    &ECDSA_P384_SHA384,
-    &RSA_PKCS1_2048_8192_SHA1,
-    &RSA_PKCS1_2048_8192_SHA256,
-    &RSA_PKCS1_2048_8192_SHA384,
-    &RSA_PKCS1_2048_8192_SHA512,
-    &RSA_PKCS1_3072_8192_SHA384
+#[cfg(feature = "trust_anchor_util")]
+static ALL_SIGALGS: &'static [&'static webpki::SignatureAlgorithm] = &[
+    &webpki::ECDSA_P256_SHA256,
+    &webpki::ECDSA_P256_SHA384,
+    &webpki::ECDSA_P384_SHA256,
+    &webpki::ECDSA_P384_SHA384,
+    &webpki::RSA_PKCS1_2048_8192_SHA1,
+    &webpki::RSA_PKCS1_2048_8192_SHA256,
+    &webpki::RSA_PKCS1_2048_8192_SHA384,
+    &webpki::RSA_PKCS1_2048_8192_SHA512,
+    &webpki::RSA_PKCS1_3072_8192_SHA384
 ];
 
 /* Checks we can verify netflix's cert chain.  This is notable
@@ -40,16 +38,16 @@ pub fn netflix()
     let inter = include_bytes!("netflix/inter.der");
     let ca = include_bytes!("netflix/ca.der");
 
-    let ee_input = Input::from(ee);
-    let inter_vec = vec![ Input::from(inter) ];
+    let ee_input = untrusted::Input::from(ee);
+    let inter_vec = vec![ untrusted::Input::from(inter) ];
     let anchors = vec![
-        trust_anchor_util::cert_der_as_trust_anchor(
-            Input::from(ca)
+        webpki::trust_anchor_util::cert_der_as_trust_anchor(
+            untrusted::Input::from(ca)
         ).unwrap()
     ];
-    let anchors = TLSServerTrustAnchors(&anchors);
+    let anchors = webpki::TLSServerTrustAnchors(&anchors);
 
-    let time = Time::from_seconds_since_unix_epoch(1492441716);
+    let time = webpki::Time::from_seconds_since_unix_epoch(1492441716);
 
     let cert = webpki::EndEntityCert::from(ee_input).unwrap();
     cert.verify_is_valid_tls_server_cert(ALL_SIGALGS, &anchors,
@@ -60,16 +58,16 @@ pub fn netflix()
 #[test]
 fn read_root_with_zero_serial() {
     let ca = include_bytes!("misc/serial_zero.der");
-    trust_anchor_util::cert_der_as_trust_anchor(
-        Input::from(ca)
+    webpki::trust_anchor_util::cert_der_as_trust_anchor(
+        untrusted::Input::from(ca)
     ).expect("godaddy cert should parse as anchor");
 }
 
 #[test]
 fn read_root_with_neg_serial() {
     let ca = include_bytes!("misc/serial_neg.der");
-    trust_anchor_util::cert_der_as_trust_anchor(
-        Input::from(ca)
+    webpki::trust_anchor_util::cert_der_as_trust_anchor(
+        untrusted::Input::from(ca)
     ).expect("idcat cert should parse as anchor");
 }
 
@@ -78,5 +76,5 @@ fn read_root_with_neg_serial() {
 fn time_constructor() {
     use std;
 
-    Time::try_from(std::time::SystemTime::now()).unwrap();
+    webpki::Time::try_from(std::time::SystemTime::now()).unwrap();
 }
