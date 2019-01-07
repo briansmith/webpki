db0c6cbd1e9404d1e325cb023b529b5542cf1b0e
Brian Smith <brian@briansmith.org>
2019-01-07 12:25:14 -1000

    Revert "Remove support for SHA-1."
    This reverts commit 97d336003e2a60afe6eeae2457ba038070687246.
    
    This will be done later.

--- tests/integration.rs
+++ tests/integration.rs
@@ -46,6 +46,7 @@ static ALL_SIGALGS: &'static [&'static webpki::SignatureAlgorithm] = &[
     &webpki::ECDSA_P256_SHA384,
     &webpki::ECDSA_P384_SHA256,
     &webpki::ECDSA_P384_SHA384,
+    &webpki::RSA_PKCS1_2048_8192_SHA1,
     &webpki::RSA_PKCS1_2048_8192_SHA256,
     &webpki::RSA_PKCS1_2048_8192_SHA384,
     &webpki::RSA_PKCS1_2048_8192_SHA512,
